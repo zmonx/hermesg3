ed006be43bd20c8fc61af000b9a9e9ce551061ad
zmonx <chumpitak@gmail.com>
2020-04-28 18:18:21 +0700

    fix slim error >> .";charset=UTF8"

--- hermesdb/api.php
+++ hermesdb/api.php
@@ -27,9 +27,9 @@ $container = $app->getContainer();
 $container['db'] = function ($c) {
     $settings = $c->get('settings')['db'];
     $pdo = new PDO(
-        "mysql:host=" . $settings['host'] . ";dbname=" . $settings['dbname'],
+        "mysql:host=" . $settings['host'] . ";dbname=" . $settings['dbname'].";charset=UTF8",
         $settings['user'],
-        $settings['pass']
+        $settings['pass'] 
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
