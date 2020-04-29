c96e4434bec56bffa0e765fb3e3b6fd0ddd13cba
Piyamin <piyaminjaima42@gmail.com>
2020-04-30 03:31:22 +0700

    change add functionShowRoom

--- application/add_reservations.js
+++ application/add_reservations.js
@@ -17,32 +17,32 @@ $(() => {
             $("#email1").val(data["0"]["resinfo_email"]);
         })
         .fail(function(jqxhr, testStatus, error) {});
-
     $("#save").click(Save);
+
 });
 
 function showRoom() {
     //    alert('sdasdsa');
     var urlAPI = "http://localhost/hermesg3/hermesdb/api.php/room";
     $.getJSON(urlAPI, {
-            format: "json",
+            format: "json"
         })
         .done(function(data) {
             console.log(data);
             var selectionObject = document.getElementById("select");
             for (var i = 0; i < data.length; i++) {
                 var option = document.createElement("OPTION"),
-                    txt = document.createTextNode(data[i]["room_name"]);
+                    txt = document.createTextNode(data[i]['room_name']);
                 option.appendChild(txt);
-                option.setAttribute("value", data[i]["room_id"]);
+                option.setAttribute("value", data[i]['room_name']);
                 select.insertBefore(option, select.lastChild);
                 $("#element-id").val(data[i]['room_id']);
             }
         })
-        .fail(function(jqxhr, textStatus, error) {});
+        .fail(function(jqxhr, textStatus, error) {})
 }
 
 function Save() {
     var urlAPI = "http://localhost/hermesg3/hermesdb/api.php/saveaddroom" + $("#select").val() + $("#lname1").val() + $("#fname1").val() + $("#tel1").val() + $("#email1").val();
-    alert($("#select").val() + $("#lname1").val() + $("#fname1").val() + $("#tel1").val() + $("#email1").val());
+
 }
