6670190f3af1a7dd91b923beda26d57df483f415
Piyamin <piyaminjaima42@gmail.com>
2020-05-09 03:10:56 +0700

    change func

--- application/edit_infoguest.js
+++ application/edit_infoguest.js
@@ -1,6 +1,6 @@
 $(() => {
     show_info();
-    show_info_edit
+    show_info_edit();
 });
 
 function show_info() {
@@ -27,35 +27,34 @@ function show_info() {
         })
 }
 
-// function show_info_edit() {
-//     var query = window.location.search.substring(1);
-//     var vars = query.split("=");
-//     var ID = vars[1];
-//     var urlAPI = "http://localhost/hermesg3/api.php/editguest/" + ID;
+function show_info_edit() {
+    var query = window.location.search.substring(1);
+    var vars = query.split("=");
+    var ID = vars[1];
+    var urlAPI = "http://localhost/hermes/api.php/show_info/" + ID;
 
-//     $.getJSON(urlAPI, {
-//         format: "json",
-//     })
+    $.getJSON(urlAPI, {
+        format: "json",
+    })
 
-//     .done(function(data) {
-//         console.log(data);
+    .done(function(data) {
+        console.log(data);
+        $("#fname_edit_infoguest").val(data["0"]["ginfo_first_name"]);
+        $("#lname_edit_infoguest").val(data["0"]["ginfo_last_name"]);
+        $("#passport_edit_infoguest").val(data["0"]["ginfo_passport_id"]);
+        $("#phone_edit_infoguest").val(data["0"]["ginfo_telno"]);
+        $("#bd_edit_infoguest").val(data["0"]["ginfo_birthday"]);
+        $("#nation_edit_infoguest").val(data["0"]["ginfo_nation"]);
+        $("#email_edit_infoguest").val(data["0"]["ginfo_email"]);
+        $("#sex_edit_infoguest").val(data["0"]["ginfo_sex"]);
+        $("#inbreakfast_edit_infoguest").val(data["0"]["bl_incbreakfast"]);
+        $("#breakfast_edit_infoguest").val(data["0"]["bl_breakfast"]);
+        $("#room_price_edit_infoguest").val(data["0"]["bl_price"]);
+        $("#padd_edit_infoguest").val(data["0"]["ginfo_mail_addr"]);
+        $("#badd_edit_infoguest").val(data["0"]["ginfo_comment"]);
+        $("#sex_edit_infoguest").val(data["0"]["ginfo_sex"]);
+        $("#incbreakfast_edit_infoguest").val(data["0"]["bl_incbreakfast"]);
+        $("#breakfast_edit_infoguest").val(data["0"]["bl_breakfast"]);
+    })
 
-//         $("#fname_edit_infoguest").val(data["0"]["ginfo_first_name"]);
-//         $("#lname_edit_infoguest").val(data["0"]["ginfo_last_name"]);
-//         $("#passport_edit_infoguest").val(data["0"]["ginfo_passport_id"]);
-//         $("#phone_edit_infoguest").val(data["0"]["ginfo_telno"]);
-//         $("#bd_edit_infoguest").val(data["0"]["ginfo_birthday"]);
-//         $("#nation_edit_infoguest").val(data["0"]["ginfo_nation"]);
-//         $("#email_edit_infoguest").val(data["0"]["ginfo_email"]);
-//         $("#sex_edit_infoguest").val(data["0"]["ginfo_sex"]);
-//         $("#inbreakfast_edit_infoguest").val(data["0"]["bl_incbreakfast"]);
-//         $("#breakfast_edit_infoguest").val(data["0"]["bl_breakfast"]);
-//         $("#room_price_edit_infoguest").val(data["0"]["bl_price"]);
-//         $("#padd_edit_infoguest").val(data["0"]["ginfo_mail_addr"]);
-//         $("#badd_edit_infoguest").val(data["0"]["ginfo_comment"]);
-//         $("#sex_edit_infoguest").val(data["0"]["ginfo_sex"]);
-//         $("#incbreakfast_edit_infoguest").val(data["0"]["bl_incbreakfast"]);
-//         $("#breakfast_edit_infoguest").val(data["0"]["bl_breakfast"]);
-//     })
-
-// }
+}
